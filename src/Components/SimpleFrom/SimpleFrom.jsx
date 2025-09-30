c1f54f7be007cbb63b51d35be188f4ea5afe1b75
Badsha012 <badshasheikh361@gmail.com>
2025-09-30 10:00:36 +0600

    adder input field

--- src/Components/SimpleFrom/SimpleFrom.jsx
+++ src/Components/SimpleFrom/SimpleFrom.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 const SimpleFrom = () => {
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(e.target.name.value);
+}
+
     return (
         <div>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
-                
+                <input type='text' name='name' placeholder='your name'></input>
+
+                <br></br>
+
+                <input type="submit" value="submit" />
+
+
             </form>
             
         </div>
