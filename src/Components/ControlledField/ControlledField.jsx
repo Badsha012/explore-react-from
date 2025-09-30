67da27b52e05c8433ebe7c8fde85e8280761b5b6
Badsha012 <badshasheikh361@gmail.com>
2025-09-30 19:19:26 +0600

    two commit

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ControlledField = () => {
+
+    const [password,setPassword] =useState('')
+const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log('submitted');
+
+
+}
+const handlePasswordOnChenge=e=>{
+    console.log(e.target.value)
+}
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+       <input type="email" name='email' id='' placeholder='Enter our email' required />
+       <br></br>
+       <input type="password" name='password'id='' placeholder='Password' onChange={handlePasswordOnChenge} defaultValue={password}  required />
+       <br></br>
+       <input type="submit" value="submit" />
+
+            </form>
         </div>
     );
 };
